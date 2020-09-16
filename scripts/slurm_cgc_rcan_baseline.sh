05bda2ff9482415c2c579febed074b5692e3fad4
Akash Palrecha <akashpalrecha@gmail.com>
2020-09-16 21:27:41 +0530

    adjustments to cgc_rcan script

--- scripts/slurm_cgc_rcan_baseline.sh
+++ scripts/slurm_cgc_rcan_baseline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -n 8
 #SBATCH -N 1
-#SBATCH -t 0-09:00
+#SBATCH -t 0-16:00
 #SBATCH -p seas_dgx1
 #SBATCH --exclude=seasdgx103
 #SBATCH --gres=gpu:1
@@ -23,8 +23,8 @@ cd Deblurring/EDSR-PyTorch/src/
 echo ""
 echo "---- BEGIN TRAINING ----"
 echo ""
-python main.py --model cgc_rcan --n_resgroups 5 --n_resblocks 10 --n_feats 64 --chop --scale 2 --ext sep --save_results --save cgc_rcan_baseline_x2 --epochs 100
-python main.py --model rcan     --n_resgroups 5 --n_resblocks 10 --n_feats 64 --chop --scale 2 --ext sep --save_results --save rcan_baseline_x2     --epochs 100
+python main.py --model cgc_rcan --n_resgroups 4 --n_resblocks 4 --n_feats 64 --scale 2 --ext sep --save_results --save cgc_rcan_baseline_x2 --epochs 100
+python main.py --model rcan     --n_resgroups 4 --n_resblocks 4 --n_feats 64 --scale 2 --ext sep --save_results --save rcan_baseline_x2     --epochs 100
 echo ""
 echo "---- TRAINING COMPLETE ----"
 
@@ -33,8 +33,8 @@ echo "---- BEGIN TESTING ----"
 
 echo ""
 echo "Start generating results"
-python main.py --model cgc_rcan --n_resgroups 5 --n_resblocks 10 --n_feats 64 --chop --scale 2 --ext sep --save_results --save cgc_rcan_baseline_x2/results --pre_train ../experiment/cgc_rcan_baseline_x2/model/model_best.pt --test_only --data_test Set5+Set14+B100+Urban100+Manga109+DIV2K --data_range 801-900
-python main.py --model rcan     --n_resgroups 5 --n_resblocks 10 --n_feats 64 --chop --scale 2 --ext sep --save_results --save rcan_baseline_x2/results     --pre_train ../experiment/rcan_baseline_x2/model/model_best.pt     --test_only --data_test Set5+Set14+B100+Urban100+Manga109+DIV2K --data_range 801-900
+python main.py --model cgc_rcan --n_resgroups 4 --n_resblocks 4 --n_feats 64 --scale 2 --ext sep --save_results --save cgc_rcan_baseline_x2/results --pre_train ../experiment/cgc_rcan_baseline_x2/model/model_best.pt --test_only --data_test Set5+Set14+B100+Urban100+Manga109+DIV2K --data_range 801-900
+python main.py --model rcan     --n_resgroups 4 --n_resblocks 4 --n_feats 64 --scale 2 --ext sep --save_results --save rcan_baseline_x2/results     --pre_train ../experiment/rcan_baseline_x2/model/model_best.pt     --test_only --data_test Set5+Set14+B100+Urban100+Manga109+DIV2K --data_range 801-900
 echo ""
 
 echo "Start MATLAB Testing"
