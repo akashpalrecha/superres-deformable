22192d9787f13395c1dfbb8d59cac1dbee156ec7
Akash Palrecha <akashpalrecha@gmail.com>
2020-09-29 13:30:04 +0530

    added slurm script for reverse_edsr

---
+++ scripts/slurm_reverse_edsr.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH -n 8
+#SBATCH -N 1
+#SBATCH -t 0-18:00
+#SBATCH -p seas_dgx1
+#SBATCH --exclude=seasdgx103
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32768
+#SBATCH -o /n/home00/apalrecha/lab/Deblurring/EDSR-PyTorch/experiment/slurm_output/reverse_edsr_baseline_x2_%j.out
+#SBATCH -e /n/home00/apalrecha/lab/Deblurring/EDSR-PyTorch/experiment/slurm_output/reverse_edsr_baseline_x2_%j.err
+
+echo "Settinp up env. Loading Conda, MATLAB, etc ..."
+source ~/.bashrc
+cd /n/home00/apalrecha/lab
+echo "Activating EDSR conda environment ..."
+source activate envs/edsr
+echo "Loading MATLAB ..."
+loadmatlab
+# echo "Loading cuda/10.1.243-fasrc01 ..."
+# module load cuda/10.1.243-fasrc01
+echo "Setup Done!"
+cd Deblurring/EDSR-PyTorch/src/
+echo ""
+echo "---- BEGIN TRAINING ----"
+echo ""
+python main.py --model reverse_edsr --res_scale 0.1 --scale 2 --ext sep --save_results --save reverse_edsr_baseline_x2 --epochs 100
+echo ""
+echo "---- TRAINING COMPLETE ----"
+
+echo ""
+echo "---- BEGIN TESTING ----"
+
+echo ""
+echo "Start generating results"
+python main.py --model reverse_edsr --res_scale 0.1 --scale 2 --ext sep --save_results --save reverse_edsr_baseline_x2/results --pre_train ../experiment/reverse_edsr_baseline_x2/model/model_best.pt --test_only --data_test Set5+Set14+B100+Urban100+Manga109+DIV2K --data_range 801-900
+echo ""
+
+echo "Start MATLAB Testing"
+echo ""
+cd ../scripts/matlab_evaluation
+./benchmark_eval.sh 2 reverse_edsr_baseline_x2/results
+echo ""
+
+echo "---- TESTING COMPLETE ----"
+echo ""
